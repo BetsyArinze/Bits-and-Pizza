a948ab61640768591d393a13dde0fe323025f3c7
BetsyArinze <88517681+BetsyArinze@users.noreply.github.com>
2021-08-07 12:30:26 +0100

    Sharing Content
    I added a share action provider to allow users to share content in the app with other apps. For example, it can be used to share details of a particular pizza to a friend

--- app/src/main/java/com/hfad/bitsandpizza/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizza/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v4.view.MenuItemCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         //Inflate the menu; this adds item to the app bar
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join me for pizza?");
         return super.onCreateOptionsMenu(menu);
     }
+
+    //Set the default text in the share action provider
+    private void setShareActionIntent(String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
