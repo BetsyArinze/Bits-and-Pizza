86498241c442d729b5f00cf4d5d9466d9e17cd30
BetsyArinze <88517681+BetsyArinze@users.noreply.github.com>
2021-09-07 11:31:28 +0100

    Added tab navigation
    First, I added tabs to the layout(activity_main.xml) using a TabLayout and an AppBarLayout.  Then I linked the tab layout to the view pager and also added the necessary string values

--- app/src/main/java/com/hfad/bitsandpizza/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizza/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
                 new SectionsPagerAdapter(getSupportFragmentManager());
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
+
+        //Attach the ViewPager to the TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
@@ -85,6 +90,22 @@ public class MainActivity extends AppCompatActivity {
                     return new StoresFragment();
             }
             return  null;
+        }
+
+        //Method to add text to the tabs
+        @Override
+        public CharSequence getPageTitle(int position){
+            switch (position){
+                case 0:
+                    return getResources().getText(R.string.home_tab);
+                case 1:
+                    return getResources().getText(R.string.pizza_tab);
+                case 2:
+                    return getResources().getText(R.string.pasta_tab);
+                case 3:
+                    return getResources().getText(R.string.store_tab);
+            }
+            return null;
         }
     }
 }
